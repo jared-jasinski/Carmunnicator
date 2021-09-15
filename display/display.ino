eb5d4272f80c048a1aa908f032d3c731dd8b4e5b
tuntinski <38989795+tuntinski@users.noreply.github.com>
2021-09-15 11:17:15 -0400

    switch logic

--- display/display.ino
+++ display/display.ino
@@ -4,14 +4,42 @@
 
 #define joyX A0
 #define joyY A1
+#define sw 2
 
 CRGB leds[NUM_LEDS];
+int state = HIGH;
+int previous = LOW;
+
+long time = 0;
+long debounce = 200;
+
 void setup() {
  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //Switch layer toggling USE STATE TO SELECT LAYER WHICH LAYER
+  switch_state = digitalRead(sw)
+  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
+    if (state == HIGH)
+      state = LOW;
+    else
+      state = HIGH;
+
+    time = millis();    
+  }
+  previous = reading;
+  //End of switch layer toggling logic
+
+
+
+  
   xValue = analogRead(joyX);
   yValue = analogRead(joyY);
+
+  
+
+
+  
 }
