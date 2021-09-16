48c2c5f194bd9c15fd9aaaad1f223a95080b7544
tuntinski <38989795+tuntinski@users.noreply.github.com>
2021-09-16 14:33:54 -0400

    joystick and states working

--- display/display.ino
+++ display/display.ino
@@ -1,12 +1,12 @@
-#include <FastLED.h>
+//#include <FastLED.h>
 
 #define NUM_LEDS 256
 
 #define joyX A0
 #define joyY A1
-#define joystick_button 2
+#define joystick_button 7
 
-CRGB leds[NUM_LEDS];
+//CRGB leds[NUM_LEDS];
 int state = HIGH;
 int previous = LOW;
 
@@ -18,27 +18,35 @@ int green_light_pin = 10;
 int blue_light_pin = 9;
 
 void setup() {
+    Serial.begin(9600);
+  pinMode(joyX, INPUT);
+  pinMode(joyY, INPUT);
+  pinMode(7, INPUT_PULLUP);
+  
   pinMode(red_light_pin, OUTPUT);
   pinMode(green_light_pin, OUTPUT);
   pinMode(blue_light_pin, OUTPUT);
- FastLED.addLeds<NEOPIXEL, 6 > (leds, NUM_LEDS);
+ //FastLED.addLeds<NEOPIXEL, 6 > (leds, NUM_LEDS);
 }
 
 void loop() {
-
   check_switch_state();
-
-  xValue = analogRead(joyX);
-  yValue = analogRead(joyY);
-  x = map(xValue, 0, 1023, -24, 24);
-  y = map(yValue, 0, 1023, -24, 24);
   
-  joystick_action(x,y);
+  float xValue = analogRead(joyX);
+  float yValue = analogRead(joyY);
+  float x = map(xValue, 0, 1023, -24, 24);
+  float y = map(yValue, 0, 1023, -24, 24);
+  //Serial.print("x: ");
+  //Serial.print(xValue);
+  //Serial.print("   |   y: ");
+  //Serial.println(yValue);
+  Serial.println(state);
+ joystick_action(x,y,state);
 
 }
 
   //JOYSTICK POSITION LOGIC FOR FOUR DIFFERNT POSITIONS
-void joystick_action(int x,int y){
+void joystick_action(int x,int y,int STATE){
     //LEFT POSITION
   if(x < -12 && y < 12 && y >-12){
     if(STATE){
@@ -75,14 +83,14 @@ void joystick_action(int x,int y){
 
   //Switch layer toggling
 void check_switch_state(){
-  switch_state = digitalRead(joystick_button);
-  if (switch_state == HIGH && previous == LOW && millis() - time > debounce) {
+int switch_state = digitalRead(joystick_button);
+  if (switch_state  == HIGH  && previous == LOW && millis() - time > debounce) {
     if (state == HIGH){
-      RGB_color(102, 204, 255)
+      RGB_color(20, 0, 40);
        state = LOW;
     }else{
       state = HIGH;
-      RGB_color(153, 153, 255)
+      RGB_color(0, 40, 0);
     }
  
     time = millis();    
